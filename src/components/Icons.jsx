abdf209bcac781b5b4ab82690ba94da074d86300
ZeroFhx <erickflores170404@gmail.com>
2025-10-27 12:43:29 -0500

    feat: Añadir el icono Check y su SVG correspondiente

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -82,6 +82,20 @@ export function CancelIcon ({ width = 24, height = 24, color = 'currentColor', }
   )
 }
 
+export function CheckIcon ({ width = 24, height = 24, color = 'currentColor', }) {
+  return (
+    <svg width={width} height={height} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M4 12.6111L8.92308 17.5L20 6.5'
+        stroke={color}
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
 export function DeleteIcon ({ width = 24, height = 24, color = 'currentColor', }) {
   return (
     <svg width={width} height={height} viewBox='0 0 24 24' fill='none'>
