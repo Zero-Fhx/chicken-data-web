cf50f04c7ecec0aa0ea97a74262cad890c4225f7
ZeroFhx <erickflores170404@gmail.com>
2025-10-28 13:40:29 -0500

    feat: Añadir icono de menú en el componente Icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -163,6 +163,20 @@ export function LogoIcon ({ width = 24, height = 24, color = 'currentColor' }) {
   )
 }
 
+export function MenuIcon ({ width = 24, height = 24, color = 'currentColor' }) {
+  return (
+    <svg width={width} height={height} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M4 6H20M4 12H20M4 18H20'
+        stroke={color}
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
 export function PlateIcon ({ width = 24, height = 24, color = 'currentColor' }) {
   return (
     <svg width={width} height={height} viewBox='0 0 24 24' fill='none'>
