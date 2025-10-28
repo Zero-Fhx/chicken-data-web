c188c2e199e3cffbe88174d288a878c2ff50bace
ZeroFhx <erickflores170404@gmail.com>
2025-10-28 01:20:45 -0500

    feat: Mejorar el estilo del texto en el componente Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ export function Loader ({ width = 40, height = 40, text = 'Cargando...' }) {
   return (
     <div className='loading-container'>
       <div className='loader' style={{ width, height }} />
-      <span style={{ color: '#6b7280' }}>{text}</span>
+      {text && <span className='loading-text'>{text}</span>}
     </div>
   )
 }
