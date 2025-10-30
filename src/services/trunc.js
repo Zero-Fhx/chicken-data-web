b69a3fb20a53b2fd6b2e19ac0dcf81b7953ae4cf
ZeroFhx <erickflores170404@gmail.com>
2025-10-30 16:19:28 -0500

    lint

--- src/services/trunc.js
+++ src/services/trunc.js
@@ -1,12 +1,12 @@
-export default function trunc(value, decimals = 2) {
-  const string = value.toString();
-  const decimalPointIndex = string.indexOf('.');
+export default function trunc (value, decimals = 2) {
+  const string = value.toString()
+  const decimalPointIndex = string.indexOf('.')
 
   if (decimalPointIndex === -1) {
-    return Number(string);
+    return Number(string)
   }
-  
-  const decimalLength = decimalPointIndex + 1;
-  const numStr = string.substr(0, decimalLength + decimals);
-  return Number(numStr);
+
+  const decimalLength = decimalPointIndex + 1
+  const numStr = string.substr(0, decimalLength + decimals)
+  return Number(numStr)
 }
