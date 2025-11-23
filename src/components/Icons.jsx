eeb6bdea6bd68dddac6bbc69ec26f0c4c7dad675
ZeroFhx <erickflores170404@gmail.com>
2025-11-22 20:45:05 -0500

    feat: agregar iconos de luna y sol, eliminar iconos de flecha

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -261,6 +261,20 @@ export function MenuIcon ({ width = 24, height = 24, color = 'currentColor' }) {
   )
 }
 
+export function MoonIcon ({ width = 24, height = 24, color = 'currentColor' }) {
+  return (
+    <svg width={width} height={height} viewBox='0 0 24 24' fill='none'>
+      <path
+        d='M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z'
+        stroke={color}
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
 export function RefreshIcon ({ width = 24, height = 24, color = 'currentColor', }) {
   return (
     <svg width={width} height={height} viewBox='0 0 24 24' fill='none'>
@@ -315,6 +329,20 @@ export function ShoppingCartIcon ({ width = 24, height = 24, color = 'currentCol
     <svg width={width} height={height} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
         d='M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z'
+        stroke={color}
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
+export function SunIcon ({ width = 24, height = 24, color = 'currentColor' }) {
+  return (
+    <svg width={width} height={height} viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M12 3V4M12 20V21M4 12H3M6.31412 6.31412L5.5 5.5M17.6859 6.31412L18.5 5.5M6.31412 17.69L5.5 18.5001M17.6859 17.69L18.5 18.5001M21 12H20M16 12C16 14.2091 14.2091 16 12 16C9.79086 16 8 14.2091 8 12C8 9.79086 9.79086 8 12 8C14.2091 8 16 9.79086 16 12Z'
         stroke={color}
         strokeWidth='2'
         strokeLinecap='round'
